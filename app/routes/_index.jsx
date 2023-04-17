927b33a3a5c1b4dc15aa4e61d4e7986ce12345b2
chandra <chandrapenugonda655@gmail.com>
2023-04-17 14:05:00 +0530

    test defer

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,7 +1,30 @@
-export const meta = () => {
-  return [{ title: "New Remix App" }];
+import { defer } from "@remix-run/cloudflare";
+import { Await, useLoaderData } from "@remix-run/react";
+import { Suspense } from "react";
+
+const sleep = async () => {
+  return new Promise((resolve) => setTimeout(resolve, 3000));
+};
+
+export const loader = async () => {
+  return defer({
+    data: fetch("https://jsonplaceholder.typicode.com/todos").then(
+      async (res) => {
+        await sleep();
+        return res.json();
+      }
+    ),
+  });
 };
 
 export default function Index() {
-  return <h1>Hello</h1>;
+  const { data } = useLoaderData();
+  return (
+    <section>
+      <h1>Hello world</h1>
+      <Suspense fallback={<h1>Loading..</h1>}>
+        <Await resolve={data}>{(data) => <p>{JSON.stringify(data)}</p>}</Await>
+      </Suspense>
+    </section>
+  );
 }
